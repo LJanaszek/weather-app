17ef78458e5f0b6f6f8cd94f240597fb00ba71b6
LJanaszek <lukasjanaszek@gmail.com>
2025-05-13 21:38:07 +0200

    zmiana typu swaggera

--- src/pages/api/swagger.ts
+++ src/pages/api/swagger.ts
@@ -3,9 +3,10 @@ import { createSwaggerSpec } from 'next-swagger-doc';
 import { swaggerDocument } from '../../lib/swagger';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    
   const spec = createSwaggerSpec({
     apiFolder: 'pages/api',
-    definition: swaggerDocument,
+    definition: swaggerDocument as undefined,
   });
 
   res.status(200).json(spec);
