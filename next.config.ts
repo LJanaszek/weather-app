881bff8d0d1e7f8913b7a2e3d920a1916896eaa5
LJanaszek <lukasjanaszek@gmail.com>
2025-05-19 22:12:12 +0200

    test2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  devIndicators: false
+  devIndicators: false,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  }
 };
 
 export default nextConfig;
